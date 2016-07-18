0d2fc9856d0cf86159e01cf07826487d05808b71
Justus <mj.kyalo@gmail.com>
2016-07-18 19:36:59 +0300

    add dummy test class

---
+++ app/src/test/java/com/justus/testinginandroid/Calculator.java
@@ -0,0 +1,26 @@
+package com.justus.testinginandroid;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class Calculator {
+    public double sum(double a, double b){
+        return 0;
+    }
+
+    public double subtract(double a, double b){
+        return 0;
+    }
+
+    public double divide(double a, double b){
+        return 0;
+    }
+
+    public double multiply(double a, double b){
+        return 0;
+    }
+}
